866c335994a6c3ca0b5530decffccd07ee617158
Nico <mail@wehmoen.dev>
2022-08-07 03:17:25 +0200

    Add value parser to replay cli option to use default of false

--- src/cli_args.rs
+++ src/cli_args.rs
@@ -1,12 +1,17 @@
-use std::fmt::Debug;
 use clap::Parser;
+use std::fmt::Debug;
 
 /// Better Ronin blockchain indexer
 #[derive(Parser, Debug)]
 #[clap(author = "wehmoen#0001", version, about, long_about = None)]
 pub struct Args {
     /// MongoDB connection URL
-    #[clap(short = 'u', long, value_parser, default_value = "mongodb://127.0.0.1:27017")]
+    #[clap(
+        short = 'u',
+        long,
+        value_parser,
+        default_value = "mongodb://127.0.0.1:27017"
+    )]
     pub db_uri: String,
     /// MongoDB database name
     #[clap(short = 'd', long, value_parser, default_value = "roninchain")]
@@ -15,8 +20,8 @@ pub struct Args {
     #[clap(short = 'w', long, value_parser, default_value = "ws://localhost:8546")]
     pub web3_hostname: String,
     /// Replay - Drops the entire database and starts reindexing the chain from block 0
-    #[clap(short = 'r', long, default_value_t = false)]
-    pub replay: bool
+    #[clap(short = 'r', long, value_parser, default_value_t = false)]
+    pub replay: bool,
 }
 
 pub fn parse() -> Args {
